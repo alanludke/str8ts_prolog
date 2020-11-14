e1e68ea491fb3b1bba3d7baca59cca8c825ce29d
Alan Lüdke <alanludke@gmail.com>
2020-11-14 11:22:19 -0300

    working 6x6

--- str8ts.pl
+++ str8ts.pl
@@ -1,14 +1,11 @@
 ?- use_module(library(clpfd)).
 
-problem([[_,_,_,_,_,_,_,_,_],
-         [_,_,_,_,_,3,_,8,5],
-         [_,_,1,_,2,_,_,_,_],
-         [_,_,_,5,_,7,_,_,_],
-         [_,_,4,_,_,_,1,_,_],
-         [_,9,_,_,_,_,_,_,_],
-         [5,_,_,_,_,_,_,7,3],
-         [_,_,2,_,1,_,_,_,_],
-         [_,_,_,_,4,_,_,_,9]]).
+problem([[_,_,_,_,_,4],
+         [_,_,_,_,_,1],
+         [_,_,3,_,_,_],
+         [5,_,_,_,_,_],
+         [6,_,4,_,_,_],
+         [_,1,_,_,_,_]]).
 
 main :-
         solve_problems.
@@ -21,13 +18,13 @@ solve_problems :-
 
 str8ts(Rows) :-
         /* list of lenght nine*/
-        length(Rows, 9),
+        length(Rows, 6),
         /* map each of the rows to have the same lenght as the list Rows*/
         maplist(same_length(Rows), Rows), 
         /* concatena todos os elementos */
         append(Rows, Vs),
          /* delimita o escopo dos numeros*/
-        Vs ins 1..9,
+        Vs ins 1..6,
         /* declaração basica do jogo acaba aqui*/
 
         /*mapeia todas as rows para serem distintas*/
